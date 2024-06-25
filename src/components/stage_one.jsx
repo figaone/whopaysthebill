768ef31e53e67508611d57b6c88b6a4a03bcae8c
kojo Adu-Gyamfi <adugyamfikonadu48@gmail.com>
2024-06-25 05:35:27 -0500

    add forms and button for stage 1 page

--- src/components/stage_one.jsx
+++ src/components/stage_one.jsx
@@ -1,8 +1,41 @@
+import { useRef } from "react"
+import { Button, Form, Alert } from "react-bootstrap"
+
 const Stage1 = () => {
+
+    const textInput = useRef()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const value = textInput.current.value;
+
+        /// run validation
+        /// add to list
+        textInput.current.value = ''
+
+        console.log(value)
+    }
 
     return (
         <>
-            Stage1
+            <Form onSubmit={handleSubmit} className="mt-4">
+                <Form.Group>
+                    <Form.Control
+                        type="text"
+                        placeholder="Add player name"
+                        name="player"
+                        ref={textInput}
+                    />
+                </Form.Group>
+
+                {/* show errors*/}
+
+
+                <Button className="miami" variant="primary" type="submit">
+                    Add player
+                </Button>
+            </Form>
         </>
     )
 }
